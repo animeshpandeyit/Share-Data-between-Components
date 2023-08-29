b35b5859e9c35aa6dd546a1fde19c2805e08c052
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2023-08-29 17:35:07 +0530

    Parent to Child via @Input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Share-Data-between-Components';
+
+  // kyuki hume parent aur child ke liye alag alag data chaiye iske liye hum 2 variable banayenge...
+
+  forParent: string = 'ye parent component ke liye hai....';
+
+  forChild: string =
+    'ye child component ke liye hai..... parent component se aaya hua  data';
 }
